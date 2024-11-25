124d0973f1e7fec52adb9abaa66063a59d982117
Phandrade21 <pauloandrade21@icloud.com>
2024-11-24 23:44:31 -0300

    Adicionar arquivos de projeto.

---
+++ APIAMBIPARDEV/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using APIAMBIPARDEV.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIAMBIPARDEV.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Ocorrencia> Ocorrencias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+          
+        }
+
+    }
+
+}
---
+++ APIAMBIPARDEV/Modelo/Ocorrencia.cs
@@ -0,0 +1,15 @@
+﻿namespace APIAMBIPARDEV.Modelo
+{
+    public class Ocorrencia
+    {
+        public int Id { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public DateTime DataOcorrencia { get; set; }
+        public string Descricao { get; set; }
+        public int ResponsavelAberturaId { get; set; }
+    
+        public int ResponsavelOcorrenciaId { get; set; }
+     
+        public string Conclusao { get; set; }
+    }
+}
